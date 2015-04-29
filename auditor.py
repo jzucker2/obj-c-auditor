34a75e44c6e2ff10169be80ac4c29da8338522ce
Jordan Zucker <jordan.zucker@gmail.com>
2015-04-29 13:14:31 -0700

    Collects all properties and methods of an obj c project.
    All info is stored in memory, need to output it

--- auditor.py
+++ auditor.py
@@ -28,13 +28,39 @@ def get_properties(header_file_as_string):
 	matches = properties_pattern.findall(header_file_as_string)
 	return matches
 
+def properties_and_methods_for_header_file(header_file_path):
+	header_file_string = get_string_from_header_file(header_file_path)
+	header_file_dict = {}
+	methods = get_methods(header_file_string)
+	properties = get_properties(header_file_string)
+	if (0 < len(properties)):
+		header_file_dict['properties'] = properties
+	if (0 < len(methods)):
+		header_file_dict['methods'] = methods
+	return header_file_dict
+
 def main():
 	project_folder = "/Users/jordanz/Coding/objective-c/PubNub/PubNub/PubNub"
 	print project_folder
-	matches = collect_header_files(project_folder)
-	print len(matches)
-	file_string = get_string_from_header_file('/Users/jordanz/Coding/objective-c/PubNub/PubNub/PubNub/Core/PubNub.h')
-	get_properties(file_string)
+	all_header_files = collect_header_files(project_folder)
+	print 'total header files found: ' + str(len(all_header_files))
+	# file_string = get_string_from_header_file('/Users/jordanz/Coding/objective-c/PubNub/PubNub/PubNub/Core/PubNub.h')
+	# get_properties(file_string)
+	audit_dict = {}
+	for header_file_path in all_header_files:
+		header_file_overview = properties_and_methods_for_header_file(header_file_path)
+		if (0 < len(header_file_overview.keys())):
+			header_file_name = os.path.basename(header_file_path)
+			header_file_overview['path'] = header_file_path
+			print header_file_name
+			audit_dict[header_file_name] = header_file_overview
+	for thingy in audit_dict:
+		print '------------------'
+		print thingy
+		print audit_dict[thingy]
+		print '------------------'
+
+
 
 
 if __name__ == "__main__":
